c82e0372e2ed456ea64792490c1679ed228ebad1
MariaJesusOliva <kexu.olivap@gmail.com>
2020-08-12 11:46:15 -0400

    value-converter

---
+++ Valores/temperatureConverter/app.js
@@ -0,0 +1,7 @@
+ 
+//Ask temperature in celsius
+const tInCelsius = prompt("¿Cuántos grados hay?");
+//Turn celsius into farenheit
+const tInFarenheit = ((tInCelsius*1.8) + 32);
+//Show temperature in farenheit
+console.log(tInFarenheit);

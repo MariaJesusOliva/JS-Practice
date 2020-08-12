f998e238e1ab87dcf9121c523206489f24c4990e
MariaJesusOliva <kexu.olivap@gmail.com>
2020-08-12 11:37:29 -0400

    value-age

---
+++ Valores/ageInSeconds/app.js
@@ -0,0 +1,7 @@
+
+//Ask age
+const age = prompt("¿Cuál es tu edad?");
+//Turn age into seconds
+const ageInSeconds = age*365*24*60*60;
+//Show age in seconds
+document.write("Tu edad en segundos es" + ageInSeconds);

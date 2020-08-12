09798f826ef426f1cf876392cd3c1a590620893a
MariaJesusOliva <kexu.olivap@gmail.com>
2020-08-12 12:05:09 -0400

    fix-age

--- Valores/ageInSeconds/app.js
+++ Valores/ageInSeconds/app.js
@@ -4,4 +4,4 @@ const age = prompt("¿Cuál es tu edad?");
 //Turn age into seconds
 const ageInSeconds = age*365*24*60*60;
 //Show age in seconds
-document.write("Tu edad en segundos es" + ageInSeconds);
+document.write("Tu edad en segundos es " + ageInSeconds);
